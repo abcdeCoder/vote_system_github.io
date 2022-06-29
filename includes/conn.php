46ae0f91cbb329cf133199fa2eac6444c1b46ff5
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-06-30 01:45:25 +0530

    data base Update

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+	$conn = new mysqli('lbkcZg4Gn6', 'remotemysql.com', '57yUQFULYx', 'lbkcZg4Gn6');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
