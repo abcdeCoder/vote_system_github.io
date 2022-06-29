cc876d64486d13427d2949ef1bcb5f3dbdb6a784
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-06-30 02:09:24 +0530

    databaseUpdate3

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('lbkcZg4Gn6', 'remotemysql.com', '57yUQFULYx', 'lbkcZg4Gn6');
+	$conn = new mysqli('remotemysql.com', 'lbkcZg4Gn6', '57yUQFULYx', 'lbkcZg4Gn6');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
