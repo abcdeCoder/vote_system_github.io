83bb64e0fa81b9528ce786c35c3d55c87638e7c6
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-06-30 18:49:08 +0530

    update

--- admin/home.php
+++ admin/home.php
@@ -20,29 +20,34 @@
                     <li class="active">Dashboard</li>
                 </ol>
             </section>
+
             <!-- Main content -->
             <section class="content">
                 <?php
-        if(isset($_SESSION['error'])){
+      if (isset($_SESSION['error'])) {
           echo "
             <div class='alert alert-danger alert-dismissible'>
               <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
               <h4><i class='icon fa fa-warning'></i> Error!</h4>
-              ".$_SESSION['error']."
+              " .
+              $_SESSION['error'] .
+              "
             </div>
           ";
           unset($_SESSION['error']);
-        }
-        if(isset($_SESSION['success'])){
+      }
+      if (isset($_SESSION['success'])) {
           echo "
             <div class='alert alert-success alert-dismissible'>
               <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
               <h4><i class='icon fa fa-check'></i> Success!</h4>
-              ".$_SESSION['success']."
+              " .
+              $_SESSION['success'] .
+              "
             </div>
           ";
           unset($_SESSION['success']);
-        }
+      }
       ?>
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
@@ -51,10 +56,9 @@
                         <div class="small-box bg-aqua">
                             <div class="inner">
                                 <?php
-                $sql = "SELECT * FROM positions";
-                $query = $conn->query($sql);
-
-                echo "<h3>".$query->num_rows."</h3>";
+              $sql = 'SELECT * FROM positions';
+              $query = $conn->query($sql);
+              echo '<h3>' . $query->num_rows . '</h3>';
               ?>
 
                                 <p>No. of Positions</p>
@@ -72,10 +76,9 @@
                         <div class="small-box bg-green">
                             <div class="inner">
                                 <?php
-                $sql = "SELECT * FROM candidates";
-                $query = $conn->query($sql);
-
-                echo "<h3>".$query->num_rows."</h3>";
+              $sql = 'SELECT * FROM candidates';
+              $query = $conn->query($sql);
+              echo '<h3>' . $query->num_rows . '</h3>';
               ?>
 
                                 <p>No. of Candidates</p>
@@ -93,10 +96,9 @@
                         <div class="small-box bg-yellow">
                             <div class="inner">
                                 <?php
-                $sql = "SELECT * FROM voters";
-                $query = $conn->query($sql);
-
-                echo "<h3>".$query->num_rows."</h3>";
+              $sql = 'SELECT * FROM voters';
+              $query = $conn->query($sql);
+              echo '<h3>' . $query->num_rows . '</h3>';
               ?>
 
                                 <p>Total Voters</p>
@@ -114,10 +116,9 @@
                         <div class="small-box bg-red">
                             <div class="inner">
                                 <?php
-                $sql = "SELECT * FROM votes GROUP BY voters_id";
-                $query = $conn->query($sql);
-
-                echo "<h3>".$query->num_rows."</h3>";
+              $sql = 'SELECT * FROM votes GROUP BY voters_id';
+              $query = $conn->query($sql);
+              echo '<h3>' . $query->num_rows . '</h3>';
               ?>
 
                                 <p>Voters Voted</p>
@@ -144,29 +145,39 @@
                 </div>
 
                 <?php
-        $sql = "SELECT * FROM positions ORDER BY priority ASC";
-        $query = $conn->query($sql);
-        $inc = 2;
-        while($row = $query->fetch_assoc()){
-          $inc = ($inc == 2) ? 1 : $inc+1; 
-          if($inc == 1) echo "<div class='row'>";
+      $sql = 'SELECT * FROM positions ORDER BY priority ASC';
+      $query = $conn->query($sql);
+      $inc = 2;
+      while ($row = $query->fetch_assoc()) {
+          $inc = $inc == 2 ? 1 : $inc + 1;
+          if ($inc == 1) {
+              echo "<div class='row'>";
+          }
           echo "
             <div class='col-sm-6'>
               <div class='box box-solid'>
                 <div class='box-header with-border'>
-                  <h4 class='box-title'><b>".$row['description']."</b></h4>
+                  <h4 class='box-title'><b>" .
+              $row['description'] .
+              "</b></h4>
                 </div>
                 <div class='box-body'>
                   <div class='chart'>
-                    <canvas id='".slugify($row['description'])."' style='height:200px'></canvas>
+                    <canvas id='" .
+              slugify($row['description']) .
+              "' style='height:200px'></canvas>
                   </div>
                 </div>
               </div>
             </div>
           ";
-          if($inc == 2) echo "</div>";  
-        }
-        if($inc == 1) echo "<div class='col-sm-6'></div></div>";
+          if ($inc == 2) {
+              echo '</div>';
+          }
+      }
+      if ($inc == 1) {
+          echo "<div class='col-sm-6'></div></div>";
+      }
       ?>
 
             </section>
@@ -179,18 +190,19 @@
 
     <?php include 'includes/scripts.php'; ?>
     <?php
-  $sql = "SELECT * FROM positions ORDER BY priority ASC";
-  $query = $conn->query($sql);
-  while($row = $query->fetch_assoc()){
-    $sql = "SELECT * FROM candidates WHERE position_id = '".$row['id']."'";
+$sql = 'SELECT * FROM positions ORDER BY priority ASC';
+$query = $conn->query($sql);
+while ($row = $query->fetch_assoc()) {
+
+    $sql = "SELECT * FROM candidates WHERE position_id = '" . $row['id'] . "'";
     $cquery = $conn->query($sql);
-    $carray = array();
-    $varray = array();
-    while($crow = $cquery->fetch_assoc()){
-      array_push($carray, $crow['lastname']);
-      $sql = "SELECT * FROM votes WHERE candidate_id = '".$crow['id']."'";
-      $vquery = $conn->query($sql);
-      array_push($varray, $vquery->num_rows);
+    $carray = [];
+    $varray = [];
+    while ($crow = $cquery->fetch_assoc()) {
+        array_push($carray, $crow['lastname']);
+        $sql = "SELECT * FROM votes WHERE candidate_id = '" . $crow['id'] . "'";
+        $vquery = $conn->query($sql);
+        array_push($varray, $vquery->num_rows);
     }
     $carray = json_encode($carray);
     $varray = json_encode($varray);
@@ -248,7 +260,7 @@
     });
     </script>
     <?php
-  }
+}
 ?>
 </body>
 
