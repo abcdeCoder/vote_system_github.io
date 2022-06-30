01d441b3a8483100318888221767920e4e83877e
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-01 00:45:46 +0530

    new22

--- admin/print.php
+++ admin/print.php
@@ -1,28 +1,20 @@
-<?php include 'includes/header.php'; ?>
+<?php
+include 'includes/session.php';
 
-<body>
+function generateRow($conn)
+{
+    $contents = '';
 
-
-
-
-
-    <?php
-    include 'includes/session.php';
-
-    function generateRow($conn)
-    {
-        $contents = '';
-
-        $sql = 'SELECT * FROM positions ORDER BY priority ASC';
-        $query = $conn->query($sql);
-        while ($row = $query->fetch_assoc()) {
-            $id = $row['id'];
-            $contents .=
-                '
+    $sql = 'SELECT * FROM positions ORDER BY priority ASC';
+    $query = $conn->query($sql);
+    while ($row = $query->fetch_assoc()) {
+        $id = $row['id'];
+        $contents .=
+            '
         		<tr>
         			<td colspan="2" align="center" style="font-size:15px;"><b>' .
-                $row['description'] .
-                '</b></td>
+            $row['description'] .
+            '</b></td>
         		</tr>
         		<tr>
         			<td width="80%"><b>Candidates</b></td>
@@ -30,70 +22,66 @@
         		</tr>
         	';
 
-            $sql = "SELECT * FROM candidates WHERE position_id = '$id' ORDER BY lastname ASC";
-            $cquery = $conn->query($sql);
-            while ($crow = $cquery->fetch_assoc()) {
-                $sql =
-                    "SELECT * FROM votes WHERE candidate_id = '" .
-                    $crow['id'] .
-                    "'";
-                $vquery = $conn->query($sql);
-                $votes = $vquery->num_rows;
+        $sql = "SELECT * FROM candidates WHERE position_id = '$id' ORDER BY lastname ASC";
+        $cquery = $conn->query($sql);
+        while ($crow = $cquery->fetch_assoc()) {
+            $sql =
+                "SELECT * FROM votes WHERE candidate_id = '" .
+                $crow['id'] .
+                "'";
+            $vquery = $conn->query($sql);
+            $votes = $vquery->num_rows;
 
-                $contents .=
-                    '
+            $contents .=
+                '
       				<tr>
       					<td>' .
-                    $crow['lastname'] .
-                    ', ' .
-                    $crow['firstname'] .
-                    '</td>
+                $crow['lastname'] .
+                ', ' .
+                $crow['firstname'] .
+                '</td>
       					<td>' .
-                    $votes .
-                    '</td>
+                $votes .
+                '</td>
       				</tr>
       			';
-            }
         }
-
-        return $contents;
     }
 
-    $parse = parse_ini_file('config.ini', false, INI_SCANNER_RAW);
-    $title = $parse['election_title'];
+    return $contents;
+}
 
-    require_once '../tcpdf/tcpdf.php';
-    $pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-    $pdf->SetCreator(PDF_CREATOR);
-    $pdf->SetTitle('Result: ' . $title);
-    $pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);
-    $pdf->setHeaderFont([PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN]);
-    $pdf->setFooterFont([PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA]);
-    $pdf->SetDefaultMonospacedFont('helvetica');
-    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-    $pdf->SetMargins(PDF_MARGIN_LEFT, '10', PDF_MARGIN_RIGHT);
-    $pdf->setPrintHeader(false);
-    $pdf->setPrintFooter(false);
-    $pdf->SetAutoPageBreak(true, 10);
-    $pdf->SetFont('helvetica', '', 11);
-    $pdf->AddPage();
-    $content = '';
-    $content .=
-        '
+$parse = parse_ini_file('config.ini', false, INI_SCANNER_RAW);
+$title = $parse['election_title'];
+
+require_once '../tcpdf/tcpdf.php';
+$pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetTitle('Result: ' . $title);
+$pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);
+$pdf->setHeaderFont([PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN]);
+$pdf->setFooterFont([PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA]);
+$pdf->SetDefaultMonospacedFont('helvetica');
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+$pdf->SetMargins(PDF_MARGIN_LEFT, '10', PDF_MARGIN_RIGHT);
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+$pdf->SetAutoPageBreak(true, 10);
+$pdf->SetFont('helvetica', '', 11);
+$pdf->AddPage();
+$content = '';
+$content .=
+    '
       	<h2 align="center">' .
-        $title .
-        '</h2>
+    $title .
+    '</h2>
       	<h4 align="center">Tally Result</h4>
       	<table border="1" cellspacing="0" cellpadding="3">  
       ';
-    $content .= generateRow($conn);
-    $content .= '</table>';
-    $file_path = $_SERVER['DOCUMENT_ROOT'] . '/election_result.pdf';
-    $pdf->writeHTML($content);
-    ob_end_clean();
-    $pdf->Output($file_path, 'F');
-    echo $file_path;
-    ?>
-</body>
-
-</html>
+$content .= generateRow($conn);
+$content .= '</table>';
+$file_path = $_SERVER['DOCUMENT_ROOT'] . '/election_result.pdf';
+$pdf->writeHTML($content);
+$pdf->Output($file_path, 'F');
+echo $file_path;
+?>
