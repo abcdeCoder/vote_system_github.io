a3703ba62736d4f90cec3fb86dfec7055cb5e78b
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-06-30 19:02:59 +0530

    Update print.php

--- admin/print.php
+++ admin/print.php
@@ -1,16 +1,20 @@
 <?php
-	include 'includes/session.php';
+include 'includes/session.php';
 
-	function generateRow($conn){
-		$contents = '';
-	 	
-		$sql = "SELECT * FROM positions ORDER BY priority ASC";
-        $query = $conn->query($sql);
-        while($row = $query->fetch_assoc()){
-        	$id = $row['id'];
-        	$contents .= '
+function generateRow($conn)
+{
+    $contents = '';
+
+    $sql = 'SELECT * FROM positions ORDER BY priority ASC';
+    $query = $conn->query($sql);
+    while ($row = $query->fetch_assoc()) {
+        $id = $row['id'];
+        $contents .=
+            '
         		<tr>
-        			<td colspan="2" align="center" style="font-size:15px;"><b>'.$row['description'].'</b></td>
+        			<td colspan="2" align="center" style="font-size:15px;"><b>' .
+            $row['description'] .
+            '</b></td>
         		</tr>
         		<tr>
         			<td width="80%"><b>Candidates</b></td>
@@ -18,55 +22,63 @@
         		</tr>
         	';
 
-        	$sql = "SELECT * FROM candidates WHERE position_id = '$id' ORDER BY lastname ASC";
-    		$cquery = $conn->query($sql);
-    		while($crow = $cquery->fetch_assoc()){
-    			$sql = "SELECT * FROM votes WHERE candidate_id = '".$crow['id']."'";
-      			$vquery = $conn->query($sql);
-      			$votes = $vquery->num_rows;
+        $sql = "SELECT * FROM candidates WHERE position_id = '$id' ORDER BY lastname ASC";
+        $cquery = $conn->query($sql);
+        while ($crow = $cquery->fetch_assoc()) {
+            $sql =
+                "SELECT * FROM votes WHERE candidate_id = '" .
+                $crow['id'] .
+                "'";
+            $vquery = $conn->query($sql);
 
-      			$contents .= '
+            $contents .=
+                '
       				<tr>
-      					<td>'.$crow['lastname'].", ".$crow['firstname'].'</td>
-      					<td>'.$votes.'</td>
+      					<td>' .
+                $crow['lastname'] .
+                ', ' .
+                $crow['firstname'] .
+                '</td>
+      					
       				</tr>
       			';
-
-    		}
-
         }
+    }
 
-		return $contents;
-	}
-		
-	$parse = parse_ini_file('config.ini', FALSE, INI_SCANNER_RAW);
-    $title = $parse['election_title'];
+    return $contents;
+}
 
-	require_once('../tcpdf/tcpdf.php');  
-    $pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
-    $pdf->SetCreator(PDF_CREATOR);  
-    $pdf->SetTitle('Result: '.$title);  
-    $pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
-    $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));  
-    $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));  
-    $pdf->SetDefaultMonospacedFont('helvetica');  
-    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);  
-    $pdf->SetMargins(PDF_MARGIN_LEFT, '10', PDF_MARGIN_RIGHT);  
-    $pdf->setPrintHeader(false);  
-    $pdf->setPrintFooter(false);  
-    $pdf->SetAutoPageBreak(TRUE, 10);  
-    $pdf->SetFont('helvetica', '', 11);  
-    $pdf->AddPage();  
-    $content = '';  
-    $content .= '
-      	<h2 align="center">'.$title.'</h2>
+$parse = parse_ini_file('config.ini', false, INI_SCANNER_RAW);
+$title = $parse['election_title'];
+
+require_once '../tcpdf/tcpdf.php';
+$pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetTitle('Result: ' . $title);
+$pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);
+$pdf->setHeaderFont([PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN]);
+$pdf->setFooterFont([PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA]);
+$pdf->SetDefaultMonospacedFont('helvetica');
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+$pdf->SetMargins(PDF_MARGIN_LEFT, '10', PDF_MARGIN_RIGHT);
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+$pdf->SetAutoPageBreak(true, 10);
+$pdf->SetFont('helvetica', '', 11);
+$pdf->AddPage();
+$content = '';
+$content .=
+    '
+      	<h2 align="center">' .
+    $title .
+    '</h2>
       	<h4 align="center">Tally Result</h4>
       	<table border="1" cellspacing="0" cellpadding="3">  
-      ';  
-   	$content .= generateRow($conn);  
-    $content .= '</table>';  
-    $pdf->writeHTML($content); 
-	ob_end_clean(); 
-    $pdf->Output('election_result.pdf', 'I');
+      ';
+$content .= generateRow($conn);
+$content .= '</table>';
+$pdf->writeHTML($content);
+ob_end_clean();
+$pdf->Output('election_result.pdf', 'I');
 
 ?>
